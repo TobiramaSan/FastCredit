e84341cb2778e370c5dd2609c5eae586af309460
temfa <akinfetemitope5@gmail.com>
2023-07-28 23:22:15 +0100

    Fixed hero

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,12 +1,9 @@
 import Woman from "../../assets/African Woman  1.png";
-import Coin12 from "../../assets/image 12.png";
-import Coin13 from "../../assets/image 13.png";
-import Coin9 from "../../assets/image 9.png";
+import Coin from "../../assets/coins.png";
 import Components1 from "../../assets/Component 49.png";
 import Components2 from "../../assets/Component 85.png";
 import Components3 from "../../assets/Component 50.png";
 import CBN from "../../assets/image 1.png";
-import Map from "../../assets/map.png";
 import Line from "../../assets/Line 4.png";
 import NDIC from "../../assets/image 3.png";
 import Apple from "../../assets/apple.png";
@@ -15,60 +12,51 @@ import "./hero.css";
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="hero-text-cont">
-        <div className="hero-text">
-          <h1>Seamless, fast, and efficient Banking Experience </h1>
-          <p>Fast credit is the best finance for you</p>
-        </div>
-        <div className="license">
-          <p>Licensed by CBN</p>
-          <img src={CBN} alt="" />
-        </div>
-        <div className="line">
-          <img src={Line} alt="" />
-        </div>
-        <div className="ndic">
-          <p>Deposits insured by</p>
-          <img src={NDIC} alt="" />
-        </div>
+      <div className="hero-wrapper">
+        <div className="hero-text-cont">
+          <div className="hero-text">
+            <h1>Seamless, fast, and efficient Banking Experience </h1>
+            <p>Fast credit is the best finance for you</p>
+          </div>
+          <div className="hero-license">
+            <div className="license">
+              <p>Licensed by CBN</p>
+              <img src={CBN} alt="" />
+            </div>
+            <div className="line">
+              <img src={Line} alt="" />
+            </div>
+            <div className="ndic">
+              <p>Deposits insured by</p>
+              <img src={NDIC} alt="" />
+            </div>
+          </div>
 
-        <div className="buttons">
-          <a className="button1" href="/">
-            <img src={Apple} alt="" />
-            <div>
-              <p>Download on the</p>
-              <span>App Store</span>
+          <div className="buttons">
+            <div className="button">
+              <img src={Apple} alt="" />
+              <div>
+                <p>Download on the</p>
+                <span>App Store</span>
+              </div>
             </div>
-          </a>
-          <a className="button2" href="/">
-            <img src={Google} alt="" />
-            <div>
-              <p>Get it on</p>
-              <span>Google Play</span>
+            <div className="button">
+              <img src={Google} alt="" />
+              <div>
+                <p>Get it on</p>
+                <span>Google Play</span>
+              </div>
             </div>
-          </a>
+          </div>
+        </div>
+        <div className="hero-img-cont">
+          <img className="woman" src={Woman} alt="" />
+          <img src={Coin} alt="coin" className="coins" />
+          <img src={Components1} alt="component" className="component-one" />
+          <img src={Components2} alt="component" className="component-two" />
+          <img src={Components3} alt="component" className="component-three" />
         </div>
       </div>
-      <div className="hero-img-cont">
-        <img className="woman" src={Woman} alt="" />
-      </div>
-      {/* <div className="hero-container">
-        <div className="hero-text">
-          <h1>Seamless, fast, and efficient Banking Experience </h1>
-          <p>Fast credit is the best finance for you</p>
-        </div>
-      </div>
-      <div>
-        <img className="map" src={Map} alt="" />
-      </div>
-      <div>
-        <img className="coin coin9" src={Coin9} alt="" />
-        <img className="coin coin13" src={Coin13} alt="" />
-        <img className="coin coin12" src={Coin12} alt="" />
-        <img className="components img1" src={Components1} alt="" />
-        <img className="components img2" src={Components2} alt="" />
-        <img className="components img3" src={Components3} alt="" />
-      </div> */}
     </div>
   );
 };
