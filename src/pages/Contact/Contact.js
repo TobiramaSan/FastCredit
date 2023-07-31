7a14286fe7c9a939c9cce9abf088c2afe89c5bc4
temfa <akinfetemitope5@gmail.com>
2023-07-31 20:58:54 +0100

    Done

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Contact = () => {
-  return <div className="div">Hello World</div>;
+  return (
+    <div className="div">
+      <h2>Hello World</h2>
+    </div>
+  );
 };
 
 export default Contact;
