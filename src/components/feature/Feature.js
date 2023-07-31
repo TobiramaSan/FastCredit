29349bf99f8b86fcfc88f24afa323f9e13288673
TobiramaSan <alademehintobi1@gmail.com>
2023-07-31 16:24:18 +0100

    added investment and loan feature

--- src/components/feature/Feature.js
+++ src/components/feature/Feature.js
@@ -1,6 +1,8 @@
 import "./feature.css";
 import Iphone from "../../assets/iPhone 12 Pro Max.png";
 import Dot from "../../assets/Ellipse 316.png";
+import investIphone from '../../assets/iPhone 12 Invest.png';
+import buyIphone from '../../assets/iPhone 12 Buy.png';
 import { useState } from "react";
 const Features = () => {
   const [feature, setFeature] = useState("Loans");
@@ -78,9 +80,81 @@ const Features = () => {
             </div>
           </div>
         ) : feature === "Investment" ? (
-          <p>Investment</p>
+          <div className="components">
+          <div className="comp-cont">
+            <div className="img">
+              <img src={investIphone} alt="" />
+            </div>
+            <div className="texts">
+              <div className="text-cont1">
+                <p>Put your money to work while earning returns.</p>
+              </div>
+              <div>
+                <div className="text-cont2">
+                  <span>Features</span>
+                  <div className="list-text">
+                    <ul>
+                      <li>
+                        <img src={Dot} alt="" className="dot" />
+                        <p className="small-texts">
+                          Flexible interest rates up to 15%
+                        </p>
+                      </li>
+                      <li>
+                        <img src={Dot} alt="" className="dot" />
+                        <p className="small-texts">No hidden charges</p>
+                      </li>
+                      <li>
+                        <img src={Dot} alt="" className="dot" />
+                        <p className="small-texts">
+                          Tenure ranges from 90 to 365 days
+                        </p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         ) : feature === "Buy" ? (
-          <p>Buy Now Pay Later</p>
+          <div className="components">
+          <div className="comp-cont">
+            <div className="img">
+              <img src={buyIphone} alt="" />
+            </div>
+            <div className="texts">
+              <div className="text-cont1">
+                <p>Access more products and pay in installments</p>
+              </div>
+              <div>
+                <div className="text-cont2">
+                  <span>Features</span>
+                  <div className="list-text">
+                    <ul>
+                      <li>
+                        <img src={Dot} alt="" className="dot" />
+                        <p className="small-texts">
+                          No hidden charges
+                        </p>
+                      </li>
+                      <li>
+                        <img src={Dot} alt="" className="dot" />
+                        <p className="small-texts">No interest rates</p>
+                      </li>
+                      <li>
+                        <img src={Dot} alt="" className="dot" />
+                        <p className="small-texts">
+                          Fast delivery of products
+                        </p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         ) : null}
       </div>
     </div>
